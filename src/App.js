a25c0d9e93d306b6ae900ec89b3d7c26bc69ae4a
Xolmer <xolmer@Xolmers-MacBook-Pro.local>
2022-04-30 17:27:02 +0100

    Modal toggle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 function App() {
+  const [cartIShown, setCartIShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIShown(true);
+  };
+  const hideCartHandler = () => {
+    setCartIShown(false);
+  };
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,7 +3,7 @@ import classes from './Modal.module.css';
 import ReactDom from 'react-dom';
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop}></div>;
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
 };
 const ModalOverlay = (props) => {
   return (
@@ -12,11 +12,16 @@ const ModalOverlay = (props) => {
     </div>
   );
 };
+
 const portalElement = document.getElementById('overlays');
+
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDom.createPortal(<Backdrop />, portalElement)}
+      {ReactDom.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
       {ReactDom.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
