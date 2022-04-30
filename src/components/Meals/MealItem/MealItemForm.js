b346d173d31bf5c3221aca36dff2ffe642bab914
Xolmer <xolmer@Xolmers-MacBook-Pro.local>
2022-04-30 14:32:41 +0100

    Meal Setup

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Fragment } from 'react';
+
+const MealItemForm = (props) => {};
+
+export default MealItemForm;
---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Meals</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </section>
+  );
+};
+
+export default MealsSummary;
